e9819d68d23ddf9cd619f8d78abe92efbbec0372
KevMain <kevmain@outlook.com>
2012-08-23 14:41:26 +0100

    Exception Handling Added closes #1

---
+++ Infrastructure/CustomFatalException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CompatibleSoftware.Infrastructure
+{
+    [Serializable]
+    public class CustomFatalException : Exception
+    {
+        private readonly String _localizedMessage;
+
+        public CustomFatalException(String message)
+            : base(message)
+        {
+            _localizedMessage = message;
+        }
+
+        public CustomFatalException(String message, Exception innerException)
+            : base(message, innerException)
+        {
+            _localizedMessage = message;
+        }
+
+        protected CustomFatalException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        public String LocalizedMessage { get { return _localizedMessage; } }
+    }
+}
---
+++ Infrastructure/Exceptions/DefaultExceptionPolicy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Threading;
+using CompatibleSoftware.Infrastructure.Logging;
+using CompatibleSoftware.Infrastructure.Security;
+using CompatibleSoftware.Infrastructure.UI.Notify;
+
+namespace CompatibleSoftware.Infrastructure.Exceptions
+{
+    public class DefaultExceptionPolicy : IExceptionPolicy
+    {
+        public ILogger Log { get; set; }
+        private readonly INotifier _notifier;
+
+        public DefaultExceptionPolicy()
+        {
+            Log = Logger.Instance;
+        }
+
+        public DefaultExceptionPolicy(INotifier notifier)
+        {
+            _notifier = notifier;
+            Log = Logger.Instance;
+        }
+
+        public bool HandleException(object sender, Exception exception)
+        {
+            if (IsFatal(exception))
+            {
+                return false;
+            }
+
+            Log.Error(exception, "An unexpected exception was caught");
+
+            do
+            {
+                RaiseNotification(exception);
+                exception = exception.InnerException;
+            } while (exception != null);
+
+            return true;
+        }
+
+        private static bool IsFatal(Exception exception)
+        {
+            return
+                exception is CustomSecurityException ||
+                exception is CustomFatalException ||
+                exception is StackOverflowException ||
+                exception is AccessViolationException ||
+                exception is AppDomainUnloadedException ||
+                exception is ThreadAbortException ||
+                exception is SecurityException ||
+                exception is SEHException;
+        }
+
+        private void RaiseNotification(Exception exception)
+        {
+            if (_notifier == null)
+            {
+                return;
+            }
+            if (exception is CustomException)
+            {
+                _notifier.Error((exception as CustomException).LocalizedMessage);
+            }
+            else
+            {
+                _notifier.Error(exception.Message);
+            }
+        }
+    }
+}
